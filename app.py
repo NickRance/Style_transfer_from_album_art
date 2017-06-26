54ed3829fe6ffde1b890c0ed0160c942278cb87d
Nick Rance <nicholas.rance@gmail.com>
2017-06-26 01:43:33 -0400

    qq

--- app.py
+++ app.py
@@ -17,15 +17,25 @@ app = Flask(__name__)
 q = Queue(connection=conn)
 #scheduler = Scheduler(connection=conn)
 
+def emptyDirectory(path):
+    filesToRemove = [f for f in os.listdir(path)]
+    for f in filesToRemove:
+        os.remove(os.path.join(path,f))
+
 
 def controller():
+    #The default number of running jobs is 4 on heroku
     print("jobs"+str(q.jobs))
-    if len(q.job_ids)==4:
+    if len(q.job_ids)==4 and not os.listdir('images/output/'): #If there are no extra jobs and the output directory is empty render the form
         #return("4 jobs in the q")
         return render_template("my-form.html")
-    else:
+    elif len(q.jobs_ids)>= 4: #If there is currently a job running render the DJ image
         #return("More than 0 jobs in the q")
         return(send_file("images/styles/DJ.jpg",mimetype='image/jpg'))
+    elif len(q.job_ids)==4 and os.listdir('images/output/'):#If there are no extra jobs running and there is a value in the output directory
+        urls = [f for f in os.listdir("images/output/")]
+        return render_template("show_images.html", urls=urls)
+
 
 def wait60s():
     time.sleep(60)
@@ -41,19 +51,19 @@ def my_form():
 @app.route('/', methods=['POST'])
 #Post requests add a job to the queue
 def my_form_post():
-    return("Post call")
+    #return("Post call")
     #q.enqueue(wait60s)
-    # text = request.form['text']
-    # imageName = text.split("/")[-1]
-    # contentImagePath = "images/input/"+imageName
-    # outputImagePath = "images/output/"+imageName
-    # # print(file_path)
-    # if not os.path.exists(outputImagePath):
-    #     f = open(contentImagePath, 'wb')
-    #     f.write(requests.get(text).content)
-    #     f.close()
-    #     #style_transfer("images/profile.jpg")
-    #     #result = q.enqueue(style_transfer,"sourceImagePath=contentImagePath,outputPath=outputImagePath, filterPath=images/styles/darksideofthemoon.jpeg")
+     text = request.form['text']
+     imageName = text.split("/")[-1]
+     contentImagePath = "images/input/"+imageName
+     outputImagePath = "images/output/"+imageName
+     # print(file_path)
+     if not os.path.exists(outputImagePath):
+        f = open(contentImagePath, 'wb')
+        f.write(requests.get(text).content)
+        f.close()
+        #style_transfer("images/profile.jpg")
+        result = q.enqueue(style_transfer,"sourceImagePath=contentImagePath,outputPath=outputImagePath, filterPath=images/styles/darksideofthemoon.jpeg")
     #     # scheduler.schedule(
     #     #     scheduled_time=datetime.utcnow(),  # Time for first execution, in UTC timezone
     #     #     func=style_transfer,  # Function to be queued
@@ -82,5 +92,6 @@ def my_form_post():
 
 
 if __name__ == '__main__':
+    emptyDirectory('/images/output')
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
