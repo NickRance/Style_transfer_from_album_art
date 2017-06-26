7d026690565ea53d1aa8f80fac73662763be9122
Nick Rance <nicholas.rance@gmail.com>
2017-06-26 00:39:37 -0400

    qqqqqqqqqqqqqqq

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from flask import render_template
 from flask import send_file
 from model import style_transfer
 import requests
+import datetime
 from rq import Queue
+#from rq_scheduler import Scheduler
 from worker import conn
 import time
 
@@ -12,13 +14,19 @@ import os
 
 app = Flask(__name__)
 q = Queue(connection=conn)
+#scheduler = Scheduler(connection=conn)
+
 
 
 @app.route('/')
 def my_form():
-    return render_template("my-form.html")
+    #schedule_controller()
+    while(True):
+        controller()
+        time.sleep(30)
 
 @app.route('/', methods=['POST'])
+#Post requests add a job to the queue
 def my_form_post():
     text = request.form['text']
     imageName = text.split("/")[-1]
@@ -30,15 +38,37 @@ def my_form_post():
         f.write(requests.get(text).content)
         f.close()
         #style_transfer("images/profile.jpg")
-        result = q.enqueue(style_transfer,"sourceImagePath=contentImagePath,outputPath=outputImagePath, filterPath=images/styles/darksideofthemoon.jpeg")
+        #result = q.enqueue(style_transfer,"sourceImagePath=contentImagePath,outputPath=outputImagePath, filterPath=images/styles/darksideofthemoon.jpeg")
+        # scheduler.schedule(
+        #     scheduled_time=datetime.utcnow(),  # Time for first execution, in UTC timezone
+        #     func=style_transfer,  # Function to be queued
+        #     args=[],  # Arguments passed into function when executed
+        #     kwargs={"sourceImagePath":contentImagePath,"outputPath":outputImagePath,"filterPath":"images/styles/darksideofthemoon.jpeg"},  # Keyword arguments passed into function when executed
+        #     interval=None,  # Time before the function is called again, in seconds
+        #     repeat=0
+        # )
+    return(send_file("images/styles/darksideofthemoon.jpeg"))
         #time.sleep(900)
-        print("result.result: "+str(result.result))
-        while(result.result==None):
-            print("Waiting for style transfer to conclude")
-            time.sleep(10)
-    #return result
-    return send_file(outputImagePath,mimetype='image/jpg')
+    #     print("result.result: "+str(result.result))
+    #     while(result.result==None):
+    #         print("Waiting for style transfer to conclude")
+    #         time.sleep(10)
+    # #return result
+    # return send_file(outputImagePath,mimetype='image/jpg')
 
+
+# def schedule_controller():
+#     #If there are no jobs queued render the url form
+#     if not scheduler.get_jobs():
+#         return render_template("my-form.html")
+#     #If a job is in progress render an image
+#     else if
+
+def controller():
+    if len(q.jobs_id)==0:
+        return render_template("my-form.html")
+    else:
+        return(send_file("images/styles/darksideofthemoon.jpeg"))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
