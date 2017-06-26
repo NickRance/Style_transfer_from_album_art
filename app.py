6abb507d3946b4b5bb2b54fcf76550e6caacc30d
Nick Rance <nicholas.rance@gmail.com>
2017-06-26 01:51:28 -0400

    qqqqq

--- app.py
+++ app.py
@@ -93,6 +93,6 @@ def my_form_post():
 
 
 if __name__ == '__main__':
-    emptyDirectory('/images/output')
+    emptyDirectory('images/output/')
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
